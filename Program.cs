56fe451a24cf1b207d447c0f37efe8113a3dfb4c
Cherepashhka <valkunariks@gmail.com>
2024-03-06 08:55:58 +0200

    init

--- Program.cs
+++ Program.cs
@@ -1,22 +1,25 @@
-﻿string s = File.ReadAllText(@"E:\L9\L1.txt");
-List<string> list = new List<string>();
-list.AddRange(s.Split(' '));
-for (int i = 0; i < list.Count; i++)
-{
-    list[i] = list[i].ToLower();
-}
-for (int i = 0; i < list.Count; i++)
-{
-    while (i != list.LastIndexOf(list[i]))
-    {
-        list.RemoveAt(list.LastIndexOf(list[i]));
-    }
-}
-foreach (string word in list)
-{
-    Console.Write(word + " ");
-}
-Console.WriteLine();
+﻿using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+//string s = File.ReadAllText(@"E:\L9\L1.txt");
+//List<string> list = new List<string>();
+//list.AddRange(s.Split(' '));
+//for (int i = 0; i < list.Count; i++)
+//{
+//    list[i] = list[i].ToLower();
+//}
+//for (int i = 0; i < list.Count; i++)
+//{
+//    while (i != list.LastIndexOf(list[i]))
+//    {
+//        list.RemoveAt(list.LastIndexOf(list[i]));
+//    }
+//}
+//foreach (string word in list)
+//{
+//    Console.Write(word + " ");
+//}
+//Console.WriteLine();
 
 
 
@@ -49,24 +52,28 @@ Console.WriteLine();
 //}
 //Console.SetCursorPosition(Console.GetCursorPosition().Left - 1, Console.GetCursorPosition().Top);
 //Console.WriteLine("}");
+//string json = JsonConvert.SerializeObject(finald);
+//File.WriteAllText("E:\\L9\\dict.json", json);
 
 
 
 
-
-//Dictionary<string, int> Food = new Dictionary<string, int>();
-//Food.Add("Apple", 60);
-//Food.Add("Banana", 100);
-//Food.Add("Meat", 250);
-//Food.Add("Cacao", 70);
-//var GroupOfLess100 = Food.Where(kp => kp.Value <= 100);
-//var GroupOfGreater100 = Food.Where(kp => kp.Value > 100);
-//foreach (var group in GroupOfLess100)
-//{
-//    Console.WriteLine(group);
-//}
-//Console.WriteLine();
-//foreach (var group in GroupOfGreater100)
-//{
-//    Console.WriteLine(group);
-//}
+Dictionary<string, int> Food = new Dictionary<string, int>();
+Food.Add("Apple", 60);
+Food.Add("Banana", 100);
+Food.Add("Meat", 250);
+Food.Add("Cacao", 70);
+Food.Add("Sugar", 120);
+Food.Add("Carrot", 90);
+var GroupOfLess100 = Food.Where(kp => kp.Value <= 100);
+var GroupOfGreater100 = Food.Where(kp => kp.Value > 100);
+Console.WriteLine("Group 1 (less than 100):");
+foreach (var group in GroupOfLess100)
+{
+    Console.WriteLine(group);
+}
+Console.WriteLine("\nGroup 2 (greater than 100):");
+foreach (var group in GroupOfGreater100)
+{
+    Console.WriteLine(group);
+}
